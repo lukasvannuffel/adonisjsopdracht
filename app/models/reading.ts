0232ad45afb73eb4eb9af81d6bba5e7cbf8a2513
Lukas Van Nuffel <lukasvannuffel02@gmail.com>
2025-10-06 11:24:59 +0200

    readingmodel

--- app/models/reading.ts
+++ app/models/reading.ts
@@ -1,13 +1,27 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@adonisjs/lucid/orm'
+import { BaseModel, column, belongsTo } from '@adonisjs/lucid/orm'
+import type { BelongsTo } from '@adonisjs/lucid/types/relations'
+import Sensor from './sensor.js'
 
 export default class Reading extends BaseModel {
   @column({ isPrimary: true })
   declare id: number
+
+  @column()
+  declare value: number
+
+  @column.dateTime()
+  declare timestamp: DateTime
+
+  @column()
+  declare sensorId: number
 
   @column.dateTime({ autoCreate: true })
   declare createdAt: DateTime
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   declare updatedAt: DateTime
+
+  @belongsTo(() => Sensor)
+  declare sensor: BelongsTo<typeof Sensor>
 }
